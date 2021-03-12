c67955085fe0e7b1071d4df9dc24aae479915c01
bryantobing12 <bryantobing0@gmail.com>
2021-03-12 09:53:14 +0700

    Fix: Configuration

--- endpoints/index.js
+++ endpoints/index.js
@@ -1,5 +1,5 @@
 export const CARI_KAMPUS =
-  'http://universities.hipolabs.com/search?country=indonesia';
+  'https://universities.hipolabs.com/search?country=indonesia';
 
 export const CARI_BEASISWA =
   'https://scholarship-api.herokuapp.com/scholarships';
